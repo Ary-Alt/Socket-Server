b1a4760b1d62e2051fa52b7a5ef051b849444113
Ari <michelortiz726@gmail.com>
2022-07-22 13:51:34 -0500

    Obtencion de usuarios conectados

--- routes/router.ts
+++ routes/router.ts
@@ -1,24 +1,34 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, response } from "express";
 import Server from '../classes/server';
+import { usuariosConectados } from '../sockets/sockets';
+import { Socket } from 'socket.io';
 export const router = Router();
+
+const sockets:any = null;
 
 
 router.get('/mensajes', (req: Request, res: Response) =>
 {
-    res.json(
+
+   
+   
+   res.json(
  {
     ok:true,
     mensaje: 'Todo esta bien'
  });
-})
+});
 
 router.post('/mensajes', (req:Request, res:Response) =>
 {
    const cuerpo = req.body.cuerpo;
    const de = req.body.de;
-   const payload = {cuerpo, de}
+
+   const payload = { cuerpo, de};
+
    const server = Server.instance;
-   server.io.emit('mensaje-nuevo', payload)
+   server.io.emit('mensaje-nuevo', payload);
+   
 
    res.json({
       ok:true,
@@ -31,29 +41,43 @@ router.post('/mensajes', (req:Request, res:Response) =>
 
 router.post('/mensajes/:para', (req:Request, res:Response) =>
 {
-   const cuerpo = req.body.cuerpo
-   const de = req.body.de
-   const para = req.params.para; 
-
+   const cuerpo = req.body.cuerpo;
+   const de = req.body.de;
+   const para = req.params.para;
+   
    const payload = {
       de,
       cuerpo
    }
-
-   const server = Server.instance;
-
-   server.io.in(para).emit('mensaje-privado', payload);
    
+   const server = Server.instance; 
+   server.io.in(para).emit('mensaje-privado', payload);
 
    res.json({
       ok:true,
       cuerpo,
-      de,
+      de, 
       para
-  
+     
    });
 
 });
 
-export default router;
+// servicio para obtener todos los IDs de los usuarios
+router.get('/usuarios', (req: Request, res: Response)=>
 
+{
+   
+   const server = Server.instance;
+   server.io.fetchSockets().then((sockets) =>  {
+      const clients: Object[] = [];
+      sockets.forEach(socket => clients.push(socket.id));
+      res.json({ ok:true, clients,});}).catch(error =>
+         res.json({ ok:true, error,}));
+      
+      });
+
+
+  
+
+export default router;
